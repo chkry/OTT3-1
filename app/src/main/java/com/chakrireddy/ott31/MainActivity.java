9a11066d675c557f4fb812db71f01aceb18ab375
Chakri Reddy <chkryreddy@gmail.com>
2017-05-19 15:50:19 +0530

    Stable Update Version 1.0

--- app/src/main/java/com/chakrireddy/ott31/MainActivity.java
+++ app/src/main/java/com/chakrireddy/ott31/MainActivity.java
@@ -1,58 +1,118 @@
 package com.chakrireddy.ott31;
 
+/*
+  //Read SharedPreferences
+       SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
+       SharedPreferences.Editor editor = sharedPref.edit();
+       editor.putString(getString(R.string.chkry_name),value1);
+       editor.commit();
+  //Write SharedPreferences
+        SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
+        String newValue = getResources().getString(R.string.chkry_name);
+        String finalValue = sharedPref.getString(getString(R.string.chkry_name),newValue);
+* */
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final TextView firstpref = (TextView) findViewById(R.id.firstImportant);
+        final TextView secondpref = (TextView) findViewById(R.id.secondImportant);
+        final TextView thirdpref = (TextView) findViewById(R.id.thirdImportant);
+
         final TextView mostImp = (TextView) findViewById(R.id.mostImportant);
-//      //  Read SharedPreferences
-//        SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
-//        SharedPreferences.Editor editor = sharedPref.edit();
-//        editor.putString(getString(R.string.chkry_name),value1);
-//        editor.commit();
+
+        firstpref.setOnClickListener(this);
+        secondpref.setOnClickListener(this);
+        thirdpref.setOnClickListener(this);
+        mostImp.setOnClickListener(this);
+
+        readAllValues("1st Important Thing", "FIRSTPREFERENCE", firstpref);
+        readAllValues("2nd Important Thing", "SECONDPREFERENCE", secondpref);
+        readAllValues("3rd Important Thing", "THIRDPREFERENCE", thirdpref);
+        readAllValues("Most Important Thing", "MOSTIMP", mostImp);
 
 
-        //Write SharedPreferences
-//        SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
-//        String newValue = getResources().getString(R.string.chkry_name);
-//        String finalValue = sharedPref.getString(getString(R.string.chkry_name),newValue);
-
-
-        SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
-        String newValue = "Most Important Value";
-        String finalValue = sharedPref.getString("MOSTIMP", newValue);
-
-        mostImp.setText(finalValue);
-
-
-        mostImp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // WriteTempValue
-                SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
-                SharedPreferences.Editor editor = sharedPref.edit();
-                String value1 = "This is updated Value";
-                editor.putString("MOSTIMP", value1);
-                editor.commit();
-
-
-                //ReadAgain
-                String newValue = "Most Important Value";
-                String finalValue = sharedPref.getString("MOSTIMP", newValue);
-                mostImp.setText(finalValue);
-            }
-        });
     }//E.O.OnCreate
 
+    @Override
+    public void onClick(View view) {
+        final int id = view.getId();
+        switch (id) {
+            case R.id.firstImportant:
+                TextView firstpref = (TextView) findViewById(R.id.firstImportant);
+                alertdialogupdater("First Important Thing", firstpref, "FIRSTPREFERENCE");
+                break;
+            case R.id.secondImportant:
+                TextView secondpref = (TextView) findViewById(R.id.secondImportant);
+                alertdialogupdater("Second Important Thing", secondpref, "SECONDPREFERENCE");
+                break;
+            case R.id.thirdImportant:
+                TextView thirdpref = (TextView) findViewById(R.id.thirdImportant);
+                alertdialogupdater("Third Important Thing", thirdpref, "THIRDPREFERENCE");
+                break;
+            case R.id.mostImportant:
+                TextView mostImp = (TextView) findViewById(R.id.mostImportant);
+                alertdialogupdater("Most Important Thing", mostImp, "MOSTIMP");
+                break;
+        }
+    }
 
+
+    public void alertdialogupdater(final String titlename, final TextView txtview, final String preferencename) {
+
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(MainActivity.this);
+        alertDialog.setTitle(titlename);
+        final EditText input = new EditText(MainActivity.this);
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT);
+        input.setLayoutParams(lp);
+        alertDialog.setView(input);
+        alertDialog.setPositiveButton("Update",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        String textinput = input.getText().toString();
+
+                        SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor = sharedPref.edit();
+                        editor.putString(preferencename, textinput);
+                        editor.apply();
+                        String finalValue = sharedPref.getString(preferencename, titlename);
+                        txtview.setText(finalValue);
+                    }
+                });
+
+        alertDialog.setNegativeButton("Cancel",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+
+        alertDialog.show();
+
+    }
+
+
+    public void readAllValues(String defaultValue, String preferenceValue, TextView txtview) {
+        SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
+        String finalValue = sharedPref.getString(preferenceValue, defaultValue);
+        txtview.setText(finalValue);
+    }
 }//E.O.Activity
